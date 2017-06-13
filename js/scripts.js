c28c0534deac908279a3a120a8bba8b28bae835e
Kirsten <Kknapp@alumni.berklee.edu>
2017-06-13 12:15:52 -0700

    Add new DRY version of jquery in js file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,19 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput = $("input#animal").val();
-    var petNameInput = $("input#petName").val();
-    var exclamation1Input = $("input#exclamation1").val();
-    var exclamation2Input = $("input#exclamation2").val();
-    var verb1Input = $("input#verb1").val();
-    var verb2Input = $("input#verb2").val();
-    var adjective1Input = $("input#adjective1").val();
-    var adjective2Input = $("input#adjective2").val();
-    var adjective3Input = $("input#adjective3").val();
-    var adjective4Input = $("input#adjective4").val();
+    var blanks = ["person1", "person2", "animal", "petName", "exclamation1", "exclamation2", "verb1", "verb2", "adjective1", "adjective2", "adjective3", "adjective4"];
 
-    $(".person1").append(person1Input);
-    $(".person2").append(person2Input);
-    $(".animal").append(animalInput);
-    $(".petName").append(petNameInput);
-    $(".exclamation1").append(exclamation1Input);
-    $(".exclamation2").append(exclamation2Input);
-    $(".verb1").append(verb1Input);
-    $(".verb2").append(verb2Input);
-    $(".adjective1").append(adjective1Input);
-    $(".adjective2").append(adjective2Input);
-    $(".adjective3").append(adjective3Input);
-    $(".adjective4").append(adjective4Input);
-
-
+    blanks.forEach(function(blank) {
+      var userInput = $("input#" + blank).val();
+      $("." + blank).text(userInput);
+    });
 
     $("#story").show();
 
     event.preventDefault();
   });
 });
+
+
+
+
+    
